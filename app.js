762fe05ebd059e2c6a2a7b467b52547cf95de64e
Umer Mehmood <deanbusiness007@gmail.com>
2024-11-23 18:05:08 +0500

    getting ready

---
+++ app.js
@@ -0,0 +1,69 @@
+
+// getting ids.
+var userName = document.getElementById('username');
+var userPassword = document.getElementById('password');
+var userEmail = document.getElementById('email');
+var userGender = document.getElementsByName('gender');
+var userCity = document.getElementById("userCity");
+
+
+function unValid(uname) {
+  if (uname.target.value.length < 5) {
+    uname.target.nextElementSibling.innerText = "atleast 5 char required";
+    uname.target.nextElementSibling.style.display = "block";
+    return;
+  }
+
+  uname.target.nextElementSibling.style.display = "none";
+}
+
+function upValid(upw) {
+  if (upw.target.value.length < 8) {
+    upw.target.nextElementSibling.innerText = "Atleast 8 char required";
+    upw.target.nextElementSibling.style.display = "block";
+    return;
+  }
+
+  upw.target.nextElementSibling.style.display = "none";
+}
+
+function ueValid(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.target.value)) {
+    mail.target.nextElementSibling.style.display = "none";
+    return;
+  } else {
+    mail.target.nextElementSibling.style.display = "block";
+    mail.target.nextElementSibling.innerText = "Invalid Email Address";
+  }
+}
+
+function Dataconstructor(name, password, email, gender) {
+  this.name = name;
+  this.password = password;
+  this.email = email;
+  this.gender = gender;
+}
+
+function usergenderChecker(){
+    for(var i=0; i < userGender.length; i++)
+    {
+        if(userGender[i].checked == true){
+            return userGender[i].value;
+        }
+    }
+}
+
+function submitData(e){
+    e.preventDefault();
+    const userData = new Dataconstructor(
+      userName.value,
+      userPassword.value,
+      userEmail.value,
+      usergenderChecker()
+    );
+
+    console.table(userData)
+}
+
+
+
